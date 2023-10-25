f93a68d8c193d8774fa3292fb21bee1258b58f24
Abraham <abrahamokaalo@gmail.com>
2023-10-25 14:41:44 +0100

    Slider and reset added

--- src/App.js
+++ src/App.js
@@ -12,18 +12,36 @@ function Counter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
   const date = new Date("feb 4 2024");
+
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
   date.setDate(date.getDate() + count);
 
   return (
-    <>
+    <div>
       <div>
-        <button onClick={() => setStep((s) => s - 1)}> - </button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span> Step: {step}</span>
+        {/* <button onClick={() => setStep((s) => s - 1)}> - </button>
         <span>Step: {step}</span>
-        <button onClick={() => setStep((s) => s + 1)}> + </button>
+        <button onClick={() => setStep((s) => s + 1)}> + </button> */}
       </div>
       <div>
         <button onClick={() => setCount((c) => c - step)}> - </button>
-        <span>Count: {count}</span>
+        {/* <span>Count: {count}</span> */}
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={() => setCount((c) => c + step)}> + </button>
       </div>
       <p>
@@ -36,7 +54,10 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
-    </>
+      {count !== 0 || step !== 1 ? (
+        <button onClick={handleReset}>Reset</button>
+      ) : null}
+    </div>
   );
 }
 export default App;
